4ba706272121bc26280a5aaab7c59f6bf38202e0
Tomasz Łukaszyk <el.moue@gmail.com>
2019-03-23 20:52:30 +0100

    Add more styling to components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default class Dashboard extends Component {
   render() {
     return (
-      <div className="container row">
+      <div className="dashboard container row">
         <div className="col s12 m6 offset-m3">
           <Advertisement />
           <Advertisement />
