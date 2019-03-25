c15ae27d64c2fc6bcdec510c9c06ce19a4993aab
Tomasz Łukaszyk <el.moue@gmail.com>
2019-03-25 20:54:20 +0100

    Add simple route guard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard";
 import SignInForm from "./components/SignInForm";
@@ -7,16 +8,38 @@ import SignUpForm from "./components/SignUpForm";
 import CreateAddForm from "./components/CreateAddForm";
 
 class App extends Component {
+  getComponent(isProtected, component) {
+    const { auth } = this.props;
+
+    if (isProtected) {
+      return auth.isEmpty ? SignInForm : component;
+    } else {
+      return auth.isEmpty ? component : Dashboard;
+    }
+  }
   render() {
     return (
       <Router>
         <div className="App">
           <Navbar />
           <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route path="/signin" component={SignInForm} />
-            <Route path="/signup" component={SignUpForm} />
-            <Route path="/create-add" component={CreateAddForm} />
+            <Route
+              exact
+              path="/"
+              component={this.getComponent(true, Dashboard)}
+            />
+            <Route
+              path="/signin"
+              component={this.getComponent(false, SignInForm)}
+            />
+            <Route
+              path="/signup"
+              component={this.getComponent(false, SignUpForm)}
+            />
+            <Route
+              path="/create-add"
+              component={this.getComponent(true, CreateAddForm)}
+            />
           </Switch>
         </div>
       </Router>
@@ -24,4 +47,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  auth: state.firebase.auth
+});
+
+export default connect(mapStateToProps)(App);
